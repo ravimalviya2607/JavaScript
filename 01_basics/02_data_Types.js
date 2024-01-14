71a36ceb9f522acdc1f85ba94b20cc26ffe74eba
Ravi Malviya <141576085+ravimalviya2607@users.noreply.github.com>
2024-01-14 18:50:11 +0530

    Create 02_data_Types.js
    Data types in js

---
+++ 01_basics/02_data_Types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Ravi")
+
+let name = "ravi" 
+let age  = 18
+let isLogged = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefinded => value will be assigned
+// symbol => unique for this
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
